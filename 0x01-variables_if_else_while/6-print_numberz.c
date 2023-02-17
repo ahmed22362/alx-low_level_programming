0d876c06908371c816496ffd0fbe3f6b09f92373
ahmed hamada <ahmedhamada496@gmail.com>
2023-02-17 14:09:17 -0800

    fix

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
  * main - Entry
- * Return: Always 0
- * */
+ * Return: always 0
+ */
 int main(void)
 {
 	int num;
-	for(num = 0; num < 10; num++)
+
+	for (num = 0; num < 10; num++)
 		putchar('0' + num);
 	putchar('\n');
 	return (0);
