a677ea57c928ba8a98169e047a7e84bb64e64f17
ahmed hamada <ahmedhamada496@gmail.com>
2023-02-17 13:39:52 -0800

    fix

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Return: ayways 0 
- * */
-
+ * main - Entry
+ * Return: Always 0
+ */
 int main(void)
 {
-	/*declare varibles*/ 
+	/*declaring variables*/
 	char l_alpha;
 	char u_alpha;
-
+	/*for loops*/
 	for (l_alpha = 'a'; l_alpha <= 'z'; l_alpha++)
-		putchar(l_alpha);
+		putchar (l_alpha);
 	for (u_alpha = 'A'; u_alpha <= 'Z'; u_alpha++)
-		putchar(u_alpha);
+		putchar (u_alpha);
 	putchar('\n');
 	return (0);
 }
