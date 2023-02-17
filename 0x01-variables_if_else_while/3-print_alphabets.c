3e91ac238885e40fc99ee970c9675fbb46f239a5
ahmed hamada <ahmedhamada496@gmail.com>
2023-02-17 13:33:11 -0800

    fix

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *
+ * main - Entry point
+ * Return: ayways 0 
+ * */
+
+int main(void)
+{
+	// declare varibles 
+	char l_alpha;
+	char u_alpha;
+
+	for(l_alpha='a'; l_alpha <= 'z'; l_alpha++)
+		putchar(l_alpha);
+	for(u_alpha='A'; u_alpha <= 'Z'; u_alpha++)
+		putchar(u_alpha);
+	putchar('\n');
+	return (0);
+}
