a81958f09c8dbe57e63e10560caa66133bf3fbf3
ahmed hamada <ahmedhamada496@gmail.com>
2023-02-17 13:55:49 -0800

    fix

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	/*declare number */
+	int n;
+	
+	for (n = 0; n < 10; n++)
+		printf("%d",n);
+	putchar('\n');
+	return (0);
+}
