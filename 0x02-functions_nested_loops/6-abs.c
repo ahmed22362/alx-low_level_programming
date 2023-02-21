03f5a8d0a6092f186c53e5174aef70257361e975
ahmed hamada <ahmedhamada496@gmail.com>
2023-02-21 10:17:25 -0800

    fix

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.c"
+/**
+ * _abs - give the absolute value of and integer
+ *
+ * @n: the given number to make it pos
+ *
+ * Return: return the absolute value of n
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -1);
+	}
+}
