c126a653a2c31ecbdfc3956c4eb133f7fdb60dfd
ahmed hamada <ahmedhamada496@gmail.com>
2023-02-21 08:16:58 -0800

    fix

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10: print the alphabet 10 times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int a = 97;
+	int x = 0;
+
+	while (x < 10)
+	{
+		while (a < 123)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+	}
+}
