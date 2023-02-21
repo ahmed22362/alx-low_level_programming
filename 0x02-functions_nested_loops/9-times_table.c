e6b240935b81e3d7dfbd9a52ee83fb1b0c710209
ahmed hamada <ahmedhamada496@gmail.com>
2023-02-21 11:25:47 -0800

    fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,8 +23,11 @@ void times_table(void)
 				_putchar((c / 10) + 48);
 				_putchar((c % 10) + 48);
 			}
-			_putchar(44);
-			_putchar(32);
+			if (b != 9)
+			{
+				_putchar(44);
+				_putchar(32);
+			}
 		}
 		_putchar('\n');
 	}
