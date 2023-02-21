5fede26b00dfbfec42f92caee9d7c5aea3eb0240
ahmed hamada <ahmedhamada496@gmail.com>
2023-02-21 11:22:13 -0800

    fix

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * times_table - print the times table
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int a, b, c;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			_putchar(44);
+			_putchar(32);
+			c = a * b;
+			if (c <= 9)
+			{
+				_putchar(32);
+				_putchar(c + 48);
+			}
+			else
+			{
+				_putchar((c / 10) + 48);
+				_putchar((c % 10) + 48);
+			}
+		}
+	}
+}
