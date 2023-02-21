816c615e09e84ebcea8b7dadf4845881ff75ee2f
ahmed hamada <ahmedhamada496@gmail.com>
2023-02-21 11:06:05 -0800

    fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,21 +7,26 @@
  */
 void jack_bauer(void)
 {
-	int h = 0;
+		int a, b, c, d;
 
-	while (h < 24)
+	for (a = 48; a <= 50; a++)
 	{
-		int m = 0;
-	
-		while (m < 60)
+		for (b = 48; b <= 57; b++)
 		{
-			_putchar('0' + h);
-			_putchar('0' + h);
-			_putchar(':');
-			_putchar('0' + m);
-			_putchar('0' + m);
-			m++;
+			for (c = 48; c <= 53; c++)
+			{
+				for (d = 48; d <= 57; d++)
+				{
+					if (a >= 50 && b >= 52)
+					break;
+					_putchar(a);
+					_putchar(b);
+					_putchar(58);
+					_putchar(c);
+					_putchar(d);
+					_putchar('\n');
+				}
+			}
 		}
-		h++;
 	}
 }
