6d753bcf88de53295f17c555e3061f3ea2a0608f
ahmed hamada <ahmedhamada496@gmail.com>
2023-02-21 08:21:53 -0800

    fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,15 @@
 #include "main.h"
 /**
- * print_alphabet_x10: print the alphabet 10 times
- *
+ * print_alphabet_x10: the wanted function
+ * Description: print the alphabet 10 times
  * Return: void
  */
 void print_alphabet_x10(void)
 {
-	int a = 97;
 	int x = 0;
 
 	while (x < 10)
-	{
+	{	int a = 97;
 		while (a < 123)
 		{
 			_putchar(a);
