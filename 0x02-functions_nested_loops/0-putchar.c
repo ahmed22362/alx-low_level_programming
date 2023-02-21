3d58a1f1ff8e3689d2b792b56db7e6588e78255b
ahmed hamada <ahmedhamada496@gmail.com>
2023-02-21 08:00:34 -0800

    fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,17 +7,18 @@
  * Description: A program pring _putchar useing putchar method
  *
  * Return: 0 Always true
- * */
+ */
+
 int main(void)
 {
 	char m[10] = "_putchar";
 	int i = 0;
 
-	while( i < 8 )
+	while (i < 8)
 	{
 		_putchar(m[i]);
 		i++;
 	}
 	_putchar('\n');
-	return(0);
+	return (0);
 }
