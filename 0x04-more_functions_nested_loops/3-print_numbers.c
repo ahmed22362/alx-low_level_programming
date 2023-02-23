dbd1ab8aea1358cc2eb38bc6ed3561f281440297
ahmed hamada <ahmedhamada496@gmail.com>
2023-02-23 10:11:55 -0800

    fix

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,19 +1,16 @@
 #include "main.h"
-
 /**
-* print_numbers - print the numbers from 0 to 9
+*print_numbers - function that print all numbers from
+*0 to 9
 *
-* Return: 0 always true
+*Return: returns nothing
 */
-
 void print_numbers(void)
 {
-	int i;
-
-	for (i = 0; i < 10; i++)
-	{
-		_putchar('0' + i);
-	}
-		_putchar('\n');
-	return (0);
+int number = 48;
+for (number = 48; number < 58; number++)
+{
+_putchar(number);
+}
+_putchar(10);
 }
