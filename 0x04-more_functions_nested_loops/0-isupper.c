44d5827de371f58a2fc3c33a7819a47d3aaca4b6
ahmed hamada <ahmedhamada496@gmail.com>
2023-02-23 08:46:48 -0800

    fix

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* _isupper - check the entered character is upper or not
+*
+* @c: the wanted to check character
+*
+* Return: 1 if the character ir upper; 0 otherwitse
+*/
+
+int _isupper(char c)
+{
+
+	if (c >= 65 && c <= 91)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
