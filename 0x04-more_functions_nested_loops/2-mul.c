be1efbb358d14b0674d4c739163f883aadf99afb
ahmed hamada <ahmedhamada496@gmail.com>
2023-02-23 10:01:31 -0800

    fix

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* mul - multypy two ginven numbers
+*
+*@a: the first number to multypy
+*@b: the second number
+*
+* Return: the result of multyply a and b
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
