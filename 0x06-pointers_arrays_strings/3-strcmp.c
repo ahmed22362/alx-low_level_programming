21aa3b5815040380a982698c9c4d81e6133298b8
ahmed hamada <ahmedhamada496@gmail.com>
2023-03-03 15:33:19 -0800

    fix

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*_strcmp - compare two strings.
+*
+*@s1: the first string.
+*@s2: the second string.
+*
+*Return: int.
+*/
+int strcmp(char *s1, char *s2)
+{
+	int i = 0, c = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0' && c == 0)
+	{
+		c = s1[i] - s2[i];
+		i++;
+	}
+	return (c);
+}
