ce351e7ef72c68c32e1fb3404e86cb14e70b5e60
ahmed hamada <ahmedhamada496@gmail.com>
2023-03-03 15:00:02 -0800

    fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,23 +8,21 @@
 *
 * Return: pointer to the concatented string
 */
-char *_strncat(char *dest, char *src, int n)
+char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	int i = 0, j = 0;
 
-	i = 0;
 	while (dest[i] != '\0')
-	{
 		i++;
-	}
-	j = 0;
-	while (j < n && src[j] != '\0')
+
+	while (src[j] != '\0')
 	{
-	dest[i] = src[j];
-	i++;
-	j++;
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
+
 	dest[i] = '\0';
+
 	return (dest);
 }
