45bb5fc083a2b68baa1c5b67c38a30f5e391b209
ahmed hamada <ahmedhamada496@gmail.com>
2023-03-15 16:43:19 -0700

    fix
    add new task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Entry point print number of argument passed to
+*
+*@argc: the count of arguments
+*@argv: vector of string contain the passed arguments
+*
+* Return: 0 - success
+*/
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
