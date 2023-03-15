f03681411e0f5605c930ff63080a62fdc609a198
ahmed hamada <ahmedhamada496@gmail.com>
2023-03-15 16:35:02 -0700

    fix
    initail commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - Entry point print it's name
+*
+*@argc: nnumber of arguments
+*@argv: vector or an array of strings
+*
+* Return: 0 - success
+*/
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
