bb6f8d7f3edffe8c82a45c6bd4a62ca220d09b1f
ahmed hamada <ahmedhamada496@gmail.com>
2023-03-15 16:56:07 -0700

    fix

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+* main - Entry point print all argment passed to
+*
+*@argc: count of argument passed to
+*@argv: vector of argument
+*
+*Return: 0 - success
+*/
+
+int main(int argc, char *argv[])
+{
+	int temp = argc;
+
+	while(argc > 0)
+	{
+		printf("%s\n", argv[temp - argc]);
+		argc--;
+	}
+	return (0);
+}
