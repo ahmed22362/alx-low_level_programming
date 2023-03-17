7742929ffe278bd33c2f4f144d25b82c5440e63b
ahmed hamada <ahmedhamada496@gmail.com>
2023-03-16 19:37:27 -0700

    fix

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,13 +11,21 @@
 int *array_range(int min, int max)
 {
 	int *block;
-	if (min > max) 
+	int i, j = 0;
+
+	if (min > max)
+		return (NULL);
+	block = malloc(sizeof(*block) * ((max - min) + 1));
+	if (block != NULL)
+	{
+		for (i = min; i <= max; i++)
+		{
+			block[j] = i;
+			j++;
+		}
+		return (block);
+	}
+	else
 		return (NULL);
 
-	block = calloc(max - min, sizeof (*block));
-	if (block == NULL)
-		return (NULL);
-
-	return (block);
 }
-
